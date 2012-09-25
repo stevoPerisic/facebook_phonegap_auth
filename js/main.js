72d00f795fc0b4dcf034b0001dece52534f15eff
Stevo Perisic <stevo.perisic@gmail.com>
2012-09-24 20:41:56 -0400

    some stuff

--- js/main.js
+++ js/main.js
@@ -5,7 +5,55 @@ function loaded() {
 
 
 function onOnline() {
- 	alert('We are online!'); 
+ 	alert('We are online!');
+	// Load the SDK Asynchronously
+  (function(d){
+	 var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	 if (d.getElementById(id)) {return;}
+	 js = d.createElement('script'); js.id = id; js.async = true;
+	 js.src = "//connect.facebook.net/en_US/all.js";
+	 ref.parentNode.insertBefore(js, ref);
+   }(document));
+   
+    // Init the SDK upon load
+	window.fbAsyncInit = function() {
+	  FB.init({
+		appId      : '519491361401353', // App ID
+		channelUrl : '//'+window.location.hostname+'/channel', // Path to your Channel File
+		status     : true, // check login status
+		cookie     : true, // enable cookies to allow the server to access the session
+		xfbml      : true  // parse XFBML
+	  });
+	  
+	  // listen for and handle auth.statusChange events
+	  FB.Event.subscribe('auth.statusChange', function(response) {
+		if (response.authResponse) {
+		  console.log(response);
+		  // user has auth'd your app and is logged into Facebook
+		  FB.api('/me', function(me){
+		  console.log(me);
+			if (me.name) {
+			  document.getElementById('auth-displayname').innerHTML = me.name;
+			}
+		  })
+		  document.getElementById('auth-loggedout').style.display = 'none';
+		  document.getElementById('auth-loggedin').style.display = 'block';
+		} else {
+		  // user has not auth'd your app, or is not logged into Facebook
+		  document.getElementById('auth-loggedout').style.display = 'block';
+		  document.getElementById('auth-loggedin').style.display = 'none';
+		}
+	  });
+
+	  // respond to clicks on the login and logout links
+	  $('#auth-loginlink').addEventListener('click', function(){
+		FB.login();
+	  });
+	  $('#auth-logoutlink').addEventListener('click', function(){
+		FB.logout();
+	  }); 
+	}
+ 
 }
  
 function onOffline() {
