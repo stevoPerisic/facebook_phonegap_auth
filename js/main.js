e1f2f414378d3366300f07008d62e4ecb62ea0ad
Stevo Perisic <stevo.perisic@gmail.com>
2012-09-26 15:53:56 -0400

    ...

--- js/main.js
+++ js/main.js
@@ -20,12 +20,6 @@ function onOnline() {
 		//this to check for fb-root
 		$('#fb-root').css({'height': 20, 'width': '100%'}).html('<p>This is the fb-root</p>');
 		
-		var e = document.createElement('script'); 
-		e.async = true;
-		e.src = 'https://connect.facebook.net/en_US/all.js';
-		$('#fb-root').appendChild(e);
-	
-		
 		window.fbAsyncInit = function() {
 			FB.init({ 
 				appId: '226909127331855', 
