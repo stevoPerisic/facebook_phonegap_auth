8ad154d75a41bb3d162f23b6548d7f4241924e97
stevoPerisic <stevo.perisic@gmail.com>
2012-09-27 08:18:30 -0400

    revert back to old code

--- js/main.js
+++ js/main.js
@@ -1,37 +1,87 @@
-function onLoad() {
-	//document.addEventListener("deviceready", onDeviceReady, false);
-	onOnline(); 
+
+function loaded() {
+	console.info('Loaded!');	
 }
+
+function getFBstuff(){
+	var token = localStorage['url'];
+	token = token.match(/=(.*?)&/);
+	console.log(token[1])
+	  
+	var url = 'https://graph.facebook.com/me/?access_token='+token[1];
+	console.log(url);  
+	  $.getJSON(url,function(json){
+			console.log(json);
+			$('#name').html(json['first_name']);
+			$('#lastname').html(json['last_name']);
+			$('#username').html(json['username']);
+			$('#city').html(json.location['name']);
+		  });		
+}
+
+
+function onOnline() {
+ 		console.info('We are online!');
+
+	  // respond to clicks on the login and logout links
+	  $('#auth-loginlink').bind('click', function(){
+		//FB.login();
+		//http://www.facebook.com/dialog/oauth/?client_id=YOUR_APP_ID&redirect_uri=YOUR_REDIRECT_URL&state=YOUR_STATE_VALUE&scope=COMMA_SEPARATED_LIST_OF_PERMISSION_NAMES
+		var thisurl = 'http://www.facebook.com/dialog/oauth/?client_id=519491361401353&&redirect_uri=http://www.facebook.com/connect/login_success.html&state=somestatevalue&response_type=token&display=touch';
+		window.plugins.childBrowser.showWebPage(thisurl);
+		window.plugins.childBrowser.onLocationChange = function (url) {
+    														console.log('childBrowser has loaded ' + url);
+															localStorage["url"] = url;
+														};
+		window.plugins.childBrowser.onClose = function () {
+    											console.info('childBrowser has closed');
+												getFBstuff();
+												};
+	  });
+	  
+	  
+	  /*$('#auth-logoutlink').addEventListener('click', function(){
+		FB.logout();
+	  }); */
+ 
+}
+ 
+function onOffline() {
+ 	alert('There are no internets!')
+}
+		
+//Used to correct android issue with news and stockprices. 
+ function onResume() {
+         var networkState = navigator.network.connection.type;
+         if (networkState == Connection.UNKNOWN || networkState == Connection.NONE) {
+                 onOffline();
+         } else {
+                 onOnline();
+         }
+		 
+}
+
+function onLoad() {
+	document.addEventListener("deviceready", onDeviceReady, false);
+}
+
 // PhoneGap is loaded and it is now safe to make calls PhoneGap methods
 //IMPORTANT: run your phonegap functions here. 
 function onDeviceReady() {
- 
+   window.plugins.childBrowser.onClose = function () {
+		onResume();
+	};
+	  
    var networkState = navigator.network.connection.type;
    if (networkState == Connection.UNKNOWN || networkState == Connection.NONE) {
-		  alert('There are no internets!');
+		   onOffline();
 		  
    } else {
 		   onOnline();	
    }
-
+	//check if they have internet connection
+	document.addEventListener("online", onOnline, false);
+	//check if they are offline
+	document.addEventListener("offline", onOffline, false);
+	loaded();
 }; //END device ready!!
-
-function onOnline() {
- 		alert('We are online!');
-		
-		
-		
-		$('#auth-loginlink').bind('click', function(e){
-			e.preventDefault();
-			FB.login(function(response) {	
-				if(response){
-						console.log(response);
-					}
-					else{
-							alert('no response');
-						}
-			});
-		});	
-		
-	
-};
